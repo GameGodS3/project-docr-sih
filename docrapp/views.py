70cc881c8b3c9c47bfa7cdb127de58c2a58e3f99
GameGodS3 <sudevssuresh@gmail.com>
2022-04-11 15:16:26 +0530

    Added: Basic views, procfile, dummy doctor api

--- docrapp/views.py
+++ docrapp/views.py
@@ -1,3 +1,45 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse("Hello, world. You're at the docrapp index.")
+
+
+def doctor(request):
+    dummy_response = {
+        "id": 2,
+        "full_name": "John Doe",
+        "age": 30,
+        "blood_type": "AB+ve",
+        "history": [
+            {
+                "date": "23 January 2022",
+                "diagnosis": "Diabetes",
+                "prescription": [
+                    {
+                        "name": "Metformin HydroChloride",
+                        "dosage": "100mg",
+                        "routine": "1-0-1",
+                        "duration": "2 weeks"
+                    },
+                    {
+                        "name": "Atorvastatin",
+                        "dosage": "100mg",
+                        "routine": "1-1-1",
+                        "duration": "2 weeks"
+                    },
+                    {
+                        "name": "Paracetamol",
+                        "dosage": "100mg",
+                        "routine": "1-1-1",
+                        "duration": "2 weeks"
+                    }
+                ]
+            }
+        ]
+    }
+
+    return JsonResponse(dummy_response)
--- healthserver/urls.py
+++ healthserver/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from docrapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('doctor/', views.doctor, name='doctor'),
 ]
