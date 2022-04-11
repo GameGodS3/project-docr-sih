2b1349cf638ef1ac4f02cca42f944618eb5702f7
GameGodS3 <sudevssuresh@gmail.com>
2022-04-11 15:40:37 +0530

    Added: CORS Origin

--- healthserver/settings.py
+++ healthserver/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["docr-sih.herokuapp.com", "127.0.0.1", "localhost", "*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Allow CORS origin for all domains
+CORS_ORIGIN_ALLOW_ALL = True
